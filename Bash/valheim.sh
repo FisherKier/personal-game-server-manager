799736689b6fd6371b8c340deb800a4467502225
Kier Fisher <fisherkier@gmail.com>
2023-12-27 13:31:08 -0800

    Change init script to use minecraft docker, add tag name extension for EC2 instance to differentiate from existing instance

--- Bash/valheim.sh
+++ Bash/valheim.sh
@@ -11,17 +11,6 @@ sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv
 sudo unzip awscliv2.zip
 sudo ./aws/install
 
-#create random string for password
-VHPW=$(echo $RANDOM | md5sum | head -c 20)
-
-#get stackname created by user data script and update SSM parameter name with this to make it unique
-STACKNAME=$(</tmp/mcParamName.txt)
-PARAMNAME=mcValheimPW-$STACKNAME
-
-#put random string into parameter store as encrypted string value
-aws ssm put-parameter --name $PARAMNAME --value $VHPW --type "SecureString" --overwrite
-
-
 #install docker and valheim app on docker
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo apt install docker-compose -y
@@ -30,30 +19,13 @@ sudo mkdir /usr/games/serverconfig
 cd /usr/games/serverconfig
 sudo bash -c 'echo "version: \"3\"
 services:
-  valheim:
-    image: mbround18/valheim:latest
+  valheim:  
+    image: itzg/minecraft-server
     ports:
-      - 2456:2456/udp
-      - 2457:2457/udp
-      - 2458:2458/udp
+      - 25565:25565
     environment:
-      - PORT=2456
-      - NAME="PapaBearsServer"
-      - WORLD="Dedicated"
-      - PASSWORD='"$VHPW"'
-      - TZ=Europe/London
-      - PUBLIC=1
-      - AUTO_UPDATE=1
-      - AUTO_UPDATE_SCHEDULE="0 3 * * *"
-      - AUTO_BACKUP=1
-      - AUTO_BACKUP_SCHEDULE="0 3 * * *"
-      - AUTO_BACKUP_REMOVE_OLD=1
-      - AUTO_BACKUP_DAYS_TO_LIVE=7
-      - AUTO_BACKUP_ON_UPDATE=1
-      - AUTO_BACKUP_ON_SHUTDOWN=1
-    volumes:
-      - ./valheim/saves:/home/steam/.config/unity3d/IronGate/Valheim
-      - ./valheim/server:/home/steam/valheim
-      - ./valheim/backups:/home/steam/backups" >> docker-compose.yml'
+      EULA: "TRUE"
+    tty: true
+    stdin_open: true >> docker-compose.yml'
 echo "@reboot root (cd /usr/games/serverconfig/ && docker-compose up)" > /etc/cron.d/awsgameserver
 sudo docker-compose up
